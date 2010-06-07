b331209f285f8dd7cb034f67fe41b9913b7d5b7e
ferruccio.barletta <ferruccio.barletta@b8a967f0-59cb-26cb-4b6e-4c4fae07dc58>
2010-06-07 14:47:25 +0000

    removed tabs

--- xml_writer.h
+++ xml_writer.h
@@ -34,143 +34,143 @@
 
 namespace xml {
 
-	class element;
+    class element;
 
-	//
-	//	xml::writer class
-	//
-	class writer
-	{
-	public:
-		// writer must be bound to an ostream
-		writer(std::ostream& os) : os(os), need_header(true) {}
-		~writer(void) { assert(elements.empty()); }
+    //
+    //	xml::writer class
+    //
+    class writer
+    {
+    public:
+        // writer must be bound to an ostream
+        writer(std::ostream& os) : os(os), need_header(true) {}
+        ~writer(void) { assert(elements.empty()); }
 
-	private:
-		std::ostream& os;				// output stream
-		bool need_header;				// have we written an XML header yet?
-		std::stack<element*> elements;	// stack of open element tags
+    private:
+        std::ostream& os;				// output stream
+        bool need_header;				// have we written an XML header yet?
+        std::stack<element*> elements;	// stack of open element tags
 
-		// write XML header, if necessary
-		writer& header() {
-			if (need_header) {
-				os << "<?xml version=\"1.0\" encoding=\"utf-8\"?>";
-				need_header = false;
-			}
-			return *this;
-		}
+        // write XML header, if necessary
+        writer& header() {
+            if (need_header) {
+                os << "<?xml version=\"1.0\" encoding=\"utf-8\"?>";
+                need_header = false;
+            }
+            return *this;
+        }
 
-		// write a single character to the output stream
-		writer& putc(char c) {
-			os.put(c);
-			return *this;
-		}
+        // write a single character to the output stream
+        writer& putc(char c) {
+            os.put(c);
+            return *this;
+        }
 
-		// write a string to the output stream
-		writer& puts(const char* str) {
-			os << str;
-			return *this;
-		}
+        // write a string to the output stream
+        writer& puts(const char* str) {
+            os << str;
+            return *this;
+        }
 
-		friend element;
-	};
+        friend element;
+    };
 
-	//
-	//	xml::element class
-	//
-	class element
-	{
-	public:
-		// create a new element tag, bound to an xml::writer
-		element(const char* name, writer& wr) : name(name), wr(wr) {
-			assert(name != 0);
-			check_parent();
-			wr.header().putc('<').puts(name);
-			tagopen = true;
-			wr.elements.push(this);
-		}
+    //
+    //	xml::element class
+    //
+    class element
+    {
+    public:
+        // create a new element tag, bound to an xml::writer
+        element(const char* name, writer& wr) : name(name), wr(wr) {
+            assert(name != 0);
+            check_parent();
+            wr.header().putc('<').puts(name);
+            tagopen = true;
+            wr.elements.push(this);
+        }
 
-		// close the current element tag
-		~element() {
-			if (!wr.elements.empty() && wr.elements.top() == this) {
-				wr.elements.pop();
-				if (tagopen)
-					wr.puts("/>");
-				else
-					wr.puts("</").puts(name).putc('>');
-			}
-		}
-		
-		// write an attribute for the current element
-		element& attr(const char* name, const char* value) {
-			assert(name != 0);
-			assert(value != 0);
-			assert(tagopen);
-			wr.putc(' ').puts(name).puts("=\"");
-			qputs(value);
-			wr.putc('"');
-			return *this;
-		}
+        // close the current element tag
+        ~element() {
+            if (!wr.elements.empty() && wr.elements.top() == this) {
+                wr.elements.pop();
+                if (tagopen)
+                    wr.puts("/>");
+                else
+                    wr.puts("</").puts(name).putc('>');
+            }
+        }
 
-		// attr() overload for std::string type
-		element& attr(const char* name, const std::string& value) { return attr(name, value.c_str()); }
+        // write an attribute for the current element
+        element& attr(const char* name, const char* value) {
+            assert(name != 0);
+            assert(value != 0);
+            assert(tagopen);
+            wr.putc(' ').puts(name).puts("=\"");
+            qputs(value);
+            wr.putc('"');
+            return *this;
+        }
 
-		// attr() function template for all streamable types
-		template <class T>
-		element& attr(const char* name, T value) {
-			std::stringstream ss;
-			ss << value;
-			attr(name, ss.str());
-			return *this;
-		}
+        // attr() overload for std::string type
+        element& attr(const char* name, const std::string& value) { return attr(name, value.c_str()); }
 
-		// write text content for the current element
-		element& contents(const char* str) {
-			assert(str != 0);
-			check_parent();
-			qputs(str);
-			return *this;
-		}
+        // attr() function template for all streamable types
+        template <class T>
+        element& attr(const char* name, T value) {
+            std::stringstream ss;
+            ss << value;
+            attr(name, ss.str());
+            return *this;
+        }
 
-		// contents() overload for std::string type
-		element& contents(const std::string& str) { return contents(str.c_str()); }
+        // write text content for the current element
+        element& contents(const char* str) {
+            assert(str != 0);
+            check_parent();
+            qputs(str);
+            return *this;
+        }
 
-		// contents() function template for all streamable types
-		template <class T>
-		element& contents(T value) {
-			std::stringstream ss;
-			ss << value;
-			contents(ss.str());
-			return *this;
-		}
+        // contents() overload for std::string type
+        element& contents(const std::string& str) { return contents(str.c_str()); }
 
-	private:
-		writer& wr;			// bound XML writer
-		const char* name;	// name of current element
-		bool tagopen;		// is the element tag for this element still open?
+        // contents() function template for all streamable types
+        template <class T>
+        element& contents(T value) {
+            std::stringstream ss;
+            ss << value;
+            contents(ss.str());
+            return *this;
+        }
 
-		// write a string quoting characters which have meaning in xml
-		element& qputs(const char* str) {
-			for (; *str; ++str)
-				switch (*str) {
-					case '&': wr.puts("&amp;"); break;
-					case '<': wr.puts("&lt;"); break;
-					case '>': wr.puts("&gt;"); break;
-					case '\'': wr.puts("&apos;"); break;
-					case '"': wr.puts("&quot;"); break;
-					default: wr.putc(*str); break;
-				}
-			return *this;
-		}
+    private:
+        writer& wr;			// bound XML writer
+        const char* name;	// name of current element
+        bool tagopen;		// is the element tag for this element still open?
 
-		// check to see if we have a parent tag which needs to be closed
-		void check_parent() {
-			if (!wr.elements.empty() && wr.elements.top()->tagopen) {
-				wr.putc('>');
-				wr.elements.top()->tagopen = false;
-			}
-		}
-	};
+        // write a string quoting characters which have meaning in xml
+        element& qputs(const char* str) {
+            for (; *str; ++str)
+                switch (*str) {
+                    case '&': wr.puts("&amp;"); break;
+                    case '<': wr.puts("&lt;"); break;
+                    case '>': wr.puts("&gt;"); break;
+                    case '\'': wr.puts("&apos;"); break;
+                    case '"': wr.puts("&quot;"); break;
+                    default: wr.putc(*str); break;
+            }
+            return *this;
+        }
+
+        // check to see if we have a parent tag which needs to be closed
+        void check_parent() {
+            if (!wr.elements.empty() && wr.elements.top()->tagopen) {
+                wr.putc('>');
+                wr.elements.top()->tagopen = false;
+            }
+        }
+    };
 
 }
 
